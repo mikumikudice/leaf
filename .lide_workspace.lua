c4ab36e304a68029fc68120f1f127cf2df5e4d32
mateusmds <mateusmoraisdias3@gmail.com>
2021-01-10 13:10:21 -0300

    removed lide temp file

--- .lide_workspace.lua
+++
@@ -1,1 +0,0 @@
-return {["type"]="leaf",["active_view"]=1,["views"]={[1]={["selection"]={[1]=60,[2]=24,[3]=60,[4]=24},["active"]=true,["text"]=false,["filename"]="leaf.lua",["type"]="doc",["scroll"]={["x"]=0,["y"]=0}}}}
